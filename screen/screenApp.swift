2f5db328893dd58d18317c3fd3e29bc9e6005ee7
ravel <ravelpotato@gmail.com>
2026-07-04 10:07:26 +0800

    Initial Commit

---
+++ screen/screenApp.swift
@@ -0,0 +1,17 @@
+//
+//  screenApp.swift
+//  screen
+//
+//  Created by T Krobot on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct screenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
